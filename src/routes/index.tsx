f281acf580ff4ccf0a7388380dbfd48c00adbc31
sandeep-kumar-270904 <trailmail123456@gmail.com>
2026-09-19 23:04:25 +0530

    Frontend Phase F1: foundation

---
+++ src/routes/index.tsx
@@ -0,0 +1,25 @@
+
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Dashboard from '../pages/Dashboard';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  },
+]);
